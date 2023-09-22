46f0af94e68d6aec86c726bd10c58b66e577ed40
Shan Rasool <28012006rasool@gmail.com>
2023-09-22 23:38:20 +0530

    created the post component and updated the dependencies, added a feature of showing profile picture in hexagonal shape when it's an nft.

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,11 +1,46 @@
 import { BsStars } from "react-icons/bs";
 import TweetBox from "./TweetBox";
-
+import Post from "./Post";
 const style = {
   wrapper: `flex-[2] border-r border-l border-[#38444d]`,
   header: `sticky top-0 bg-[#15202b] z-10 p-4 flex justify-between items-center`,
   headerTitle: `text-xl font-bold`,
 };
+
+const tweets = [
+  {
+    displayName: "Shan",
+    userName: "0xB5228D9C95be809D4e6766C0e85c21cFD7321369",
+    avatar: "",
+    text: "Hey there",
+    isProfileImageNft: true,
+    timeStamp: "2021-09-01T12:00:00.000Z",
+  },
+  {
+    displayName: "Stoic",
+    userName: "0xB5228D9C95be809D4e6766C0e85c21cFD7321369",
+    avatar: "",
+    text: "Hola Amigos",
+    isProfileImageNft: false,
+    timeStamp: "2021-09-01T12:00:00.000Z",
+  },
+  {
+    displayName: "Ali",
+    userName: "0xB5228D9C95be809D4e6766C0e85c21cFD7321369",
+    avatar: "",
+    text: "Hey Guys Its Shan",
+    isProfileImageNft: false,
+    timeStamp: "2021-09-01T12:00:00.000Z",
+  },
+  {
+    displayName: "Bob Miller",
+    userName: "0xB5228D9C95be809D4e6766C0e85c21cFD7321369",
+    avatar: "",
+    text: "Gm Web3",
+    isProfileImageNft: true,
+    timeStamp: "2021-09-01T12:00:00.000Z",
+  },
+];
 
 export default function Feed() {
   return (
@@ -15,6 +50,19 @@ export default function Feed() {
         <BsStars />
       </div>
       <TweetBox />
+      {tweets.map((tweet, index) => (
+        <Post
+          key={index}
+          displayName={tweet.displayName}
+          userName={`${tweet.userName.slice(0, 4)}...${tweet.userName.slice(
+            38
+          )}`}
+          avatar={tweet.avatar}
+          text={tweet.text}
+          isProfileImageNft={tweet.isProfileImageNft}
+          timeStamp={tweet.timeStamp}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/components/Post.js
@@ -0,0 +1,88 @@
+import React from "react";
+import Image from "next/image";
+import { format } from "timeago.js";
+import { BsFillPatchCheckFill } from "react-icons/bs";
+import { FaRegComment, FaRetweet } from "react-icons/fa";
+import { AiOutlineHeart } from "react-icons/ai";
+import { FiShare } from "react-icons/fi";
+
+const style = {
+  wrapper: `flex p-3 border-b border-[#38444d]`,
+  profileImage: `rounded-full h-[40px] w-[40px] object-cover`,
+  postMain: `flex-1 px-4`,
+  headerDetails: `flex items-center`,
+  name: `font-bold mr-1`,
+  verified: `text-[0.8rem]`,
+  handleAndTimeAgo: `text-[#8899a6] ml-1`,
+  tweet: `my-2`,
+  image: `rounded-3xl`,
+  footer: `flex justify-between mr-28 mt-4 text-[#8899a6]`,
+  footerIcon: `rounded-full text-lg p-2`,
+};
+
+const Post = ({
+  displayName,
+  userName,
+  text,
+  avatar,
+  timeStamp,
+  isProfileImageNft,
+}) => {
+  return (
+    <div className={style.wrapper}>
+      <div>
+        <Image
+          src="/user.png"
+          alt={userName}
+          className={
+            isProfileImageNft
+              ? `${style.profileImage} smallHex`
+              : style.profileImage
+          }
+          width="64"
+          height="64"
+        />
+      </div>
+      <div className={style.postMain}>
+        <div>
+          <span className={style.headerDetails}>
+            <span className={style.name}>{displayName}</span>
+            {isProfileImageNft && (
+              <span>
+                <BsFillPatchCheckFill />
+              </span>
+            )}
+            <spna className={style.handleAndTimeAgo}>
+              @{userName} • {format(new Date(timeStamp).getTime())}
+            </spna>
+          </span>
+          <div className={style.tweet}>{text}</div>
+        </div>
+        <div className={style.footer}>
+          <div
+            className={`${style.footerIcon} hover:text-[#1d9bf0] hover:bg-[#1e364a]`}
+          >
+            <FaRegComment />
+          </div>
+          <div
+            className={`${style.footerIcon} hover:text-[#03ba7c] hover:bg-[#1b393b]`}
+          >
+            <FaRetweet />
+          </div>
+          <div
+            className={`${style.footerIcon} hover:text-[#f91c80] hover:bg-[#39243c]`}
+          >
+            <AiOutlineHeart />
+          </div>
+          <div
+            className={`${style.footerIcon} hover:text-[#1d9bf0] hover:bg-[#1e364a]`}
+          >
+            <FiShare />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
